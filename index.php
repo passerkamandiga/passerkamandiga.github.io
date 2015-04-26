b8d3a7bc5f56cda48f08029a91f5143ee2a54b03
passerkamandiga <ahmadsholahuddin_z@yahoo.co.id>
2015-04-26 11:36:44 +0700

    Revert "new"
    This reverts commit 39bcf0027f77d5a25332f5c3b0635197553044ed.

--- index.php
+++ index.php
@@ -112,15 +112,32 @@
 	  		<div class="row">
 	  			<div class="col-md-offset-1 col-md-10">
 
-				<form action="MAILTO:someone@example.com" method="post" enctype="text/plain">
-					Name:<br>
-					<input type="text" name="name" value="your name"><br>
-					E-mail:<br>
-					<input type="text" name="mail" value="your email"><br>
-					Comment:<br>
-					<input type="text" name="comment" value="your comment" size="50"><br><br>
-					<input type="submit" value="Send">
-						<input type="reset" value="Reset">
+				<form class="form-horizontal" role="form">
+				  <div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+					  <input type="text" class="form-control" id="inputName" placeholder="Name">
+					</div>
+				  </div>
+				  <div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+					  <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+					</div>
+				  </div>
+				  <div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+					  <input type="text" class="form-control" id="inputSubject" placeholder="Subject">
+					</div>
+				  </div>
+				  <div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+					  <textarea name="message" class="form-control" rows="3" placeholder="Message"></textarea>
+					</div>
+				  </div>
+				  <div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+					 <button type="button" class="btn btn-theme btn-lg btn-block">Send message</button>
+					</div>
+				  </div>
 				</form>
 	
 	  			</div>
